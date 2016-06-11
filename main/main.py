cae994ada80bf81d002224c19595e5fd919ad899
Kasia Gruba <katarzyna.gruba.pl@gmail.com>
2016-06-11 21:49:02 +0200

    Added species and metabolism type

--- main/main.py
+++ main/main.py
@@ -1,12 +1,16 @@
 from model.Bacteria import Bacteria
+from model.Aerobs import Aerobs
 
 def main():
     print "Let's create new bacteria"
 
-    bacteria = Bacteria("Escherichia")
+    bacteria = Bacteria("Escherichia", "Coli")
 
     bacteria.printName()
+    bacteria.printSpecies()
 
+    aerobBacteria = Aerobs()
+    aerobBacteria.printMetabolism()
 
 if __name__ == "__main__":
     main()
