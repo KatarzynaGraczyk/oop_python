2d961ab1ca305d41615dc395bc92051ae71d69cd
Kasia Gruba <katarzyna.gruba.pl@gmail.com>
2016-06-11 21:47:06 +0200

    Added species and metabolism type into bacteria class

--- model/Bacteria.py
+++ model/Bacteria.py
@@ -1,8 +1,17 @@
 class Bacteria:
     _name = ""
+    _species = ""
+    metabolismType = ""
 
-    def __init__(self, name):
+    def __init__(self, name, species):
         self._name = name
+        self._species = species
 
     def printName(self):
         print "My name is: " + self._name
+
+    def printSpecies(self):
+        print "My species name is: " + self._species
+
+    def printMetabolism(self):
+        print "My metabolism type is: " + self.metabolismType
